49d30b66fcf579c2255f0dc5310cf2e5f68e97a5
maldron <bagjinhyeong640@gmail.com>
2023-09-20 15:12:17 +0900

    기존 코드 수정
    보기좋게 함수로 정리

--- CS/Player.gd
+++ CS/Player.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 # 속도
 const SPEED = 100.0
 # 가속도
-const ACCELERATION = 600.0
+const ACCELERATION = 800.0
 # 마찰력
-const FRICTION = 600.0
+const FRICTION = 1000.0
 # 점프 중력
 const JUMP_VELOCITY = -300.0
 
@@ -14,20 +14,32 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = move_toward(velocity.x, SPEED, ACCELERATION * delta)
-	else:
-		velocity.x = 0 
-
+	ApplyGravity(delta)
+	HandleJump()
+	
+	var input_axis = Input.get_axis("ui_left","ui_right")
+		
+	HandleAcceleration(input_axis, delta)
+	ApplyFriction(input_axis, delta)
 	move_and_slide()
+	
+func ApplyGravity(delta):
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			
+func HandleJump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	else:
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+			
+func HandleAcceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+
+func ApplyFriction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+	
